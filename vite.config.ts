134a55b54354ca782afd5a1d148f6c5de6ee261a
Elia Pellegrino <elia@piramid.studio>
2024-06-05 19:14:21 +0200

    clean up

--- vite.config.ts
+++ vite.config.ts
@@ -13,32 +13,8 @@ export default defineConfig({
   },
   plugins: [
     remix({
-      // ignoredRouteFiles: ['**/*.css'],
-      ignoredRouteFiles: ['**/.*', '**/*.test.{js,jsx,ts,tsx}'],
-      // serverModuleFormat: 'cjs',
-      // serverDependenciesToBundle: 'all',
-      // serverDependenciesToBundle: [
-      //   '@phosphor-icons/react',
-      //   'datocms-structured-text-generic-html-renderer',
-      //   'datocms-structured-text-utils',
-      //   'dequal',
-      //   'react-datocms',
-      //   'react-intersection-observer',
-      //   'use-deep-compare-effect',
-      //   'react-photoswipe-gallery',
-      //   'photoswipe',
-      //   'photoswipe/dist/photoswipe.css',
-      //   'ssr-window',
-      //   'ssr-window/ssr-window.esm.js',
-      //   'dom7',
-      //   '@uidotdev/usehooks',
-      //   '@remix-run/react',
-      //   '@remix-run/node',
-      //   /^remix-i18next/, // <-- this will bundle all remix-i18next packages
-      //   'i18next-fs-backend',
-      //   'react-gtm-module',
-      //   /^swiper/, // <-- this will bundle all swiper packages
-      // ], // to fix Error [ERR_REQUIRE_ESM]: require() of ES Module
+      ignoredRouteFiles: ['**/*.css'],
+      // ignoredRouteFiles: ['**/.*', '**/*.test.{js,jsx,ts,tsx}'],
 
       // Routes
       routes: (defineRoutes) => {

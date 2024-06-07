d2a16ebf39ab9ba69e3838c2929f3e1de58b8dd8
Elia Pellegrino <elia@piramid.studio>
2024-06-07 11:30:26 +0200

    new test route and api

---
+++ app/routes/test.tsx
@@ -0,0 +1,26 @@
+import { de } from '@faker-js/faker'
+import type { LoaderFunctionArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const url = new URL(request.url)
+  const baseUrl = url.origin
+
+  const testApi = await fetch(`${baseUrl}/api/test`).then((res) => res.json())
+
+  return { testApi }
+}
+
+const Test = () => {
+  const data = useLoaderData<typeof loader>()
+  const testApi = data.testApi
+
+  return (
+    <div>
+      <h1 className="py-16">Hello - {testApi.hello}</h1>
+    </div>
+  )
+}
+
+export default Test
